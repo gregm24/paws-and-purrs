1bd44a044457c841dff84639c0e80eb25bc5c2c7
gregm24 <144722295+gregm24@users.noreply.github.com>
2026-03-05 16:01:51 -0500

    feat: add services section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Hero from "@/src/components/sections/Hero";
 import About from "@/src/components/sections/About";
+import Services from "@/src/components/sections/Services";
 
 /*
 Homepage
 
-This is the main page that combines all sections
-of the Paws & Purrs website.
+Combines all main sections of the website.
 */
 
 export default function Home() {
@@ -15,6 +15,8 @@ export default function Home() {
       <Hero />
 
       <About />
+
+      <Services />
 
     </main>
   );
