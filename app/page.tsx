1d4f918dd5783ef2dfcb3cbf14f4b4b76a8ef939
gregm24 <144722295+gregm24@users.noreply.github.com>
2026-03-05 16:18:27 -0500

    add charity and future sections to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import Services from "@/src/components/sections/Services";
 import Booking from "@/src/components/sections/Booking";
 import Reviews from "@/src/components/sections/Reviews";
 import Gallery from "@/src/components/sections/Gallery";
+import Charities from "@/src/components/sections/Charities";
+import Future from "@/src/components/sections/Future";
 
 /*
 Homepage
-
-Combines all sections of the website.
 */
 
 export default function Home() {
@@ -28,6 +28,10 @@ export default function Home() {
       <Reviews />
 
       <Gallery />
+
+      <Charities />
+
+      <Future />
 
     </main>
 
